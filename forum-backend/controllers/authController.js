0e599f2cc34ed2326afb911596dd13decc5a76c7
Alikhairi42 <khairiabdelali5@gmail.com>
2026-07-22 12:11:55 +0200

    feat: start node.js backend, prisma setup, auth api

--- forum-backend/controllers/authController.js
+++ forum-backend/controllers/authController.js
@@ -1,5 +1,8 @@
 const prisma = require('../config/prisma');
 const bcrypt = require('bcryptjs');
+const jwt = require('jsonwebtoken');
+
+
 exports.register = async(req, res) => {
     try {
         const { username, email, password } = req.body;
@@ -22,7 +25,7 @@ exports.register = async(req, res) => {
             }
         });
         res.status(201).json({
-            message: "User registered successfully! 🎉",
+            message: "user registered successfully!",
             user: {
                 id: newUser.id,
                 username: newUser.username,
@@ -31,6 +34,41 @@ exports.register = async(req, res) => {
         });
     } catch (error) {
         console.error(error);
-        res.status(500).json({ message: "Internal server error!" });
+        res.status(500).json({ message: "server error!" });
     }
+};
+
+exports.login = async(req, res) => {
+    try {
+        const { email, password } = req.body;
+
+        const user = await prisma.user.findUnique({
+            where: { email, email }
+        });
+        if (user)
+            return res.status(404).json({ message: "This is email not found" });
+
+        const passvalid = await bcrypt.compare(password, user.password);
+        if (!passvalid)
+            return res.status(401).json({ message: "password is not correct" });
+
+        const token = jwt.sign({ userId: user.id },
+            process.env.JWT_SECRET, { expiresIn: '1d' });
+
+
+        res.status(200).json({
+            message: "succed",
+            token: token,
+            user: {
+                id: user.id,
+                username: user.username,
+                email: user.email
+            }
+        });
+
+    } catch (error) {
+        console.error(error);
+        res.status(500).json({ message: "server faile" });
+    }
+
 };
